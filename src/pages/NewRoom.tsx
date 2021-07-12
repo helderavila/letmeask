09bf63707da63a9efc672b05f75900f6995d41dd
Helder Avila <helder.avila@codeby.com.br>
2021-07-12 18:52:25 -0300

    feat: Routes and navigation

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import { Button } from '../components/Button'
 
 import illustrationImg from '../assets/images/illustration.svg'
@@ -27,7 +29,7 @@ export function NewRoom() {
             </Button>
 
           </form>
-          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+          <p>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></p>
         </div>
       </main>
     </div>
