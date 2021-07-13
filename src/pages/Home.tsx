68e3d53a5a3ea3f138f965b0b239dd826557c777
Helder Avila <helder.avila@codeby.com.br>
2021-07-13 19:32:13 -0300

    feat: Join and create a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
+import { FormEvent, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 
+import { database } from '../services/firebase'
 import { useAuth } from '../hooks/useAuth'
 
 import { Button } from '../components/Button'
@@ -15,11 +17,25 @@ export function Home() {
 
   const { signInWithGoogle, user } = useAuth()
 
+  const [roomCode, setRoomCode] = useState('')
+
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle()
     }
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault()
+
+    if (roomCode.trim() === '') return
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if (!roomRef.exists()) return alert('Room does not exists')
+    
+    history.push(`/rooms/${roomCode}`)
   }
 
   return (
@@ -39,10 +55,12 @@ export function Home() {
           <div className="separator">
             ou entre em uma sala
           </div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input 
               type="text"
-              placeholder="Digite o código da sala" 
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode} 
             />
             <Button type="submit">
               Entrar na sala
